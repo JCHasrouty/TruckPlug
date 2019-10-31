2f12a7aad2264c00540dc3bc0bc2351e9ff0d68d
JCHasrouty <jchasrouty@gmail.com>
2019-10-31 11:30:15 -0700

    :fire::rocket: Pushing new files/structure

---
+++ truck_plug/src/components/headerComponent/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import{
+    Link
+} from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+        <div className="logo">
+            <img src={require('/Users/jchasrouty/Documents/CSUN/COMP-490/TruckPlug/truck_plug/src/logo/TruckPlug_Logo.png')}/>
+        </div>
+
+        <nav>
+            <ul>
+                <li className="first">
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/mapPage">Map</Link>
+                </li>
+                <li>
+                    <Link to="/aboutPage">About Us</Link>
+                </li>
+                <li className="last">
+                    <Link to="/contactPage">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
